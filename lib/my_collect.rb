7066f965b340b4c847d00081fa6135569e3341c2
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 22:32:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,9 @@
 def my_collect(array)
   new_array = []
   i = 0
-  if array.each.length > 1
-    array.collect do |entity|
-      new_array << entitity.split(" ").first
-      i += 1
-    end
-  else
-    array.collect do |entity|
-      new_array << entity.upcase
-      i += 1
-    end
+  array.collect do |entity|
+    new_array << entity.upcase
+    i += 1
   end
   return new_array
 end
