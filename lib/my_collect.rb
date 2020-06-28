6273a53afaaf993e10ac3b1dbd45b2ef36e1624e
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 22:34:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array)
   new_array = []
   i = 0
-  array.collect do |entity|
-    new_array << entity.upcase
+  while i < array.length
+    new_array << (yield(array[i]))
     i += 1
   end
   return new_array
